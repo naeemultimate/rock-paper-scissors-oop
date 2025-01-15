fe8dad63c4c3dfa18eede861f6f59292379d3467
MashnoorJahan <mashnoorjahan2000@gmail.com>
2025-01-15 12:38:46 +0000

    Add game2.js with oop

--- game2.js
+++ game2.js
@@ -1,55 +1,64 @@
-const choices = ["rock", "paper", "scissors"];
+const prompt = require("prompt-sync")({ sigint: true });
 
 class Player {
-  constructor(){
-    this.move = null;
+  constructor(name) {
+    this.name = name;
+    this.choice = null;
   }
 }
 
-
-const p = new Player();
-p.move = "rock";
-console.log(p.move);
-
-
 class HumanPlayer extends Player {
-  makeMove(){
-    let userMove = prompt("Enter your move: rock, paper or scissors?");
-    this.move = userMove;
+  makeChoice() {
+    let humanChoice = prompt("Enter your choice: rock, paper or scissors? ");
+    this.choice = humanChoice;
   }
 }
 
 class ComputerPlayer extends Player {
-  makeMove(){
-    let computerMove = choices[Math.floor(Math.random()*3)];
-    this.move = computerMove;
+  makeChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) //random number generator
+    if (computerChoice === 0) {
+      this.choice = 'rock'
+    } else if (computerChoice === 1) {
+      this.choice = 'paper'
+    } else if (computerChoice === 2) {
+      this.choice = 'scissors'
+    }
   }
 }
 
 class Game {
-  constructor(computerPlayer, humanPlayer){
-    this.computerPlayer = computer;
-    this.humanPlayer = humanPlayer;
+  constructor() {
+    this.computerPlayer = new ComputerPlayer("Computer")
+    this.humanPlayer = new HumanPlayer("Human")
   }
 
-  makeMoves() {
-    this.computerPlayer.makeMove();
-    this.humanPlayer.makeMove();
+  play() {
+    this.computerPlayer.makeChoice();
+    this.humanPlayer.makeChoice();
+
+    const computerChoice = this.computerPlayer.choice;
+    const humanChoice = this.humanPlayer.choice;
+    console.log(`Human chose ${humanChoice} and computer chose ${computerChoice}`);
+
+    if (computerChoice === 'rock' && humanChoice === 'scissors') {
+      console.log("Rock beats Scissors so you have lost...");
+    } else if (computerChoice === 'paper' && humanChoice === 'rock') {
+      console.log("Paper beats rock so you have lost...");
+    } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+      console.log("Scissors beats paper so you have lost...");
+    } else if (computerChoice === humanChoice) {
+      console.log(`You both chose ${computerChoice} so it's a tie!`);
+    } else {
+      console.log("You won!")
+    }
   }
-
-  // showResult(){
-  //   if (computerChoice === 'rock' && humanChoice === 'scissors'){
-  //     console.log("Rock beats Scissors so you have lost...");
-  // } else if (computerChoice === 'paper' && humanChoice === 'rock'){
-  //     console.log("Paper beats rock so you have lost...");
-  // } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
-  //     console.log("Scissors beats paper so you have lost...");
-  // } else if (computerChoice === humanChoice) {
-  //   console.log(`You both chose ${computerChoice} so it's a tie!`);
-  // } else {
-  //   console.log("You won!")
-  // }
-  // }
-
 }
 
+const game = new Game();
+game.play();
+game.play();
+
+
+
+// Run: node game2.js
